9a72af1c8cba70fa6a6dd039f3de7942b477f71f
Kang <kangjianhu@conflux-chain.org>
2026-01-08 17:40:57 +0800

    stack info

--- common/recover.go
+++ common/recover.go
@@ -3,11 +3,13 @@ package common
 import (
 	"github.com/sirupsen/logrus"
 	"os"
+	"runtime/debug"
 )
 
 func Recover() {
 	if r := recover(); r != nil {
 		logrus.Error("recover panic, ", r)
+		logrus.Info(string(debug.Stack()))
 		os.Exit(-1)
 	}
 }
